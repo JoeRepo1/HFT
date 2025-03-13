0dc5fe8ab1c59527ff7f575176a4a1fa01d414cd
Joe <heumannj@outlook.com>
2025-03-13 11:33:05 +0000

    Updated

--- Src/pch.h
+++ Src/pch.h
@@ -1,54 +1,60 @@
 #pragma once  //pch.h
 
+//------ Trading
 #include <algorithm>
 #include <array>
 #include <atomic>
-#include <cassert>
 #include <chrono>
 #include <cmath>
-#include <concepts>
 #include <condition_variable>
-#include <cstdint>
-#include <ctime>
 #include <execution>
-#include <filesystem>
-#include <format>
 #include <fstream>
 #include <functional>
 #include <future>
+#include <immintrin.h>
 #include <iomanip>
 #include <iostream>
 #include <memory>
 #include <mutex>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <ranges>
-#include <shared_mutex>
 #include <span>
-#include <sstream>
-#include <stdexcept>
-#include <string>
 #include <thread>
 #include <tuple>
-#include <type_traits>
 #include <unordered_map>
-#include <utility>
 #include <vector>
+#include <xmmintrin.h>
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER)
 #include <intrin.h>
-#include <malloc.h>
-#else
+#elif defined(__GNUC__) || defined(__clang__)
 #include <cpuid.h>
-#include <cstdlib>
-#include <x86intrin.h>
 #endif
 
-#include "InitGlobals.h"
-
-//trading.cpp
 using std::abs, std::async, std::clamp, std::exp, std::make_unique, std::max, std::sqrt, std::tanh;
 using std::cin, std::cout, std::memory_order_relaxed, std::ranges::transform;
 using std::array, std::atomic, std::function, std::launch, std::pair, std::ranges::subrange,
       std::span, std::tuple, std::unique_ptr, std::unordered_map, std::vector;
+
+//------ ConnectionFactory
+#include <cstdint>
+#include <memory>
+#include <shared_mutex>
+#include <unordered_map>
+
+//------ AsyncLogger
+#include <atomic>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <thread>
+
+//------ AI
+#include <filesystem>
+#include <string>
+#include <utility>
+
+//------ Globals
+#include "InitGlobals.h"
